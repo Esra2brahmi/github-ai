d3153c1903d652ad50e0425a482fea6ad012b67b
Esra2brahmi <esrabrahmii@gmail.com>
2025-07-11 20:51:51 +0100

    first commit

---
+++ src/app/(protected)/layout.tsx
@@ -0,0 +1,27 @@
+import { UserButton } from "@clerk/nextjs"
+import { SidebarProvider } from "~/components/ui/sidebar"
+import React from 'react'
+
+type Props = {
+    children: React.ReactNode
+}
+
+const SidebarLayout =({ children }: Props) => {
+    return (
+        <SidebarProvider>
+            <main className="w-full m-2">
+                <div className="flex items-center gap-2 border-sidebar-border bg-sidebar border shadow rounded-md p-2 px-4">
+                    <div className="ml-auto"></div>
+                    <UserButton/>
+                </div>
+                <div className="h-4">
+                    <div className="border-sidebar-border bg-sidebar border shadow rounded-md overflow-y-scroll h-[calc(100vh-6rem)] p-4">
+                        {children}
+                    </div>
+                </div>
+            </main>
+        </SidebarProvider>
+    )
+}
+
+export default SidebarLayout
