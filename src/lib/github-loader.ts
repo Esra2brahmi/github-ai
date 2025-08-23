0d65144b278077a456ff6e7b39c3b74bd96d6813
Esra2brahmi <esrabrahmii@gmail.com>
2025-08-23 16:21:49 +0100

    push last changes

--- src/lib/github-loader.ts
+++ src/lib/github-loader.ts
@@ -2,6 +2,19 @@ import { GithubRepoLoader} from '@langchain/community/document_loaders/web/githu
 
 export const loadGithubRepo = async (githubUrl: string,githubToken?: string) => {
     const loader = new GithubRepoLoader(githubUrl, {
-        
+        accessToken: githubToken || '',
+        branch: 'main',
+        recursive: true,
+        ignoreFiles: ['.gitignore', 'README.md','package-lock.json', 'yarn.lock','pnpm-lock.yaml'],
+        unknown:'warn',
+        maxConcurrency: 5,
     })
+    const docs = await loader.load();
+    return docs
+}
+
+
+export const indexGithubRepo = async (projectId: string , githubUrl: string , githubToken?: string) => {
+    const docs = await loadGithubRepo(githubUrl, githubToken);
+    const allEmbeddings = await generateEmbeddings(docs)
 }
