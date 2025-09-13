be601527c7b616de9ba2f7470654849e7b9b856c
Esra2brahmi <esrabrahmii@gmail.com>
2025-09-13 16:41:57 +0100

    save it

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,5 @@
-import { Button } from "~/components/ui/button";
+import { redirect } from 'next/navigation'
 
-
-export default async function Home() {
-  return <Button>Click me</Button>
-  
+export default function Home() {
+  redirect('/dashboard')
 }
